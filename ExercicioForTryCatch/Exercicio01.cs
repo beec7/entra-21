d45a694240302ae7b384211d4bef1c25db2ad5b8
leonardo <Suporte@BLUE11>
2022-05-13 21:52:18 -0300

    final da aula

---
+++ ExercicioForTryCatch/Exercicio01.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioForTryCatch
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                var nomeValdio = false;
+                while (nomeValdio == false)
+                {
+                    try
+                    {
+                        Console.Write("Nome da peça: ");
+                        var nome = Console.ReadLine();
+                        if (nome.Length > 3)
+                        {
+                            nomeValdio = true;
+                        }
+                        else
+                        {
+                            Console.Write("Nome deve conter mais do que 3 letras");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write("Caralho vc esta fazendo merda");
+                    }
+
+                }
+                var valorValido = false;
+                while (valorValido == false)
+                {
+                    try
+                    {
+                        Console.Write("Valor da peça:");
+                        var valor = Convert.ToDouble(Console.ReadLine());
+                        if (valor <= 0)
+                        {
+                            Console.Write("Valor não pode ser negativo");
+                        }
+                        else if (valor < 100000000.0)
+                        {
+                            Console.Write("Valor excedeu o limite ");
+                        }
+                        else
+                        {
+                            valorValido = true;
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write("Valor invalido, Digite novamente");
+                    }
+                }
+
+            }
+        }
+    }
+}
--- ExercicioForTryCatch/Program.cs
+++ ExercicioForTryCatch/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var loop = true;
+
+while (loop == true)
+{
+    var opcaoValida = false;
+    var opcaoDesejada = 0;
+    while (opcaoValida == false)
+    {
+        try
+        {
+            Console.Write("Informe a opção desejada");
+            opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+            if(opcaoDesejada <= 0)
+            {
+                Console.Write("Não tem opção negaiva:");
+            }
+            else
+            {
+                opcaoValida = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write("Informe o número da opção");
+        }
+    }
+
+}
