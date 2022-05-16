4c46052f27740243d5fae5ebd3d88538601bf594
beec7 <leonardo.pokrewecky@gmail.com>
2022-05-16 13:27:50 -0300

    Meuno For

---
+++ ExercicioForTryCatch/Exercicio02.cs
@@ -0,0 +1,6 @@
+﻿internal class Exercicio02
+{
+    public Exercicio02()
+    {
+    }
+}
--- ExercicioForTryCatch/Program.cs
+++ ExercicioForTryCatch/Program.cs
@@ -1,19 +1,32 @@
-﻿
+﻿using ExercicioForTryCatch;
 var loop = true;
 
+
+//fazer as questões: 1, 5, 8, 10, 12, 13
+//da lista 3
 while (loop == true)
 {
     var opcaoValida = false;
     var opcaoDesejada = 0;
     while (opcaoValida == false)
     {
+        Console.Clear();
+        Console.Write("" +
+            "1 - Exercicio 01 While\n" +
+            "2 - Exercicio 05 While\n" +
+            "3 - Exercicio 08 While\n" +
+            "4 - Exercicio 10 While\n" +
+            "5 - Exercicio 12 While\n" +
+            "6 - Exercicio 13 While\n" +
+            "99 - Sair");
         try
         {
             Console.Write("Informe a opção desejada");
             opcaoDesejada = Convert.ToInt32(Console.ReadLine());
-            if(opcaoDesejada <= 0)
+            if (opcaoDesejada <= 0)
             {
                 Console.Write("Não tem opção negaiva:");
+                Console.ReadKey();
             }
             else
             {
@@ -23,6 +36,22 @@ while (loop == true)
         catch (Exception ex)
         {
             Console.Write("Informe o número da opção");
+            Console.ReadKey();
+        }
+
+
+        if (opcaoDesejada != 99)
+        {
+            if (opcaoDesejada == 1)
+            {
+                var exercicio01 = new Exercicio01();
+                exercicio01.Executar();
+            }
+
+        }
+        else
+        {
+            loop = false;
         }
     }
 
