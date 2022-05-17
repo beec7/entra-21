81abca8bec790b5fc7872e45b3edf69c8afd7c52
leonardo <leonardo.pokrewecky@gmail.com>
2022-05-16 22:10:02 -0300

    Exercicio 03

--- ExercicioForTryCatch/Exercicio03.cs
+++ ExercicioForTryCatch/Exercicio03.cs
@@ -10,7 +10,27 @@ namespace ExercicioForTryCatch
     {
         public void Executar()
         {
+            Console.Clear();
+            var validar = false;
 
+            while (validar == false)
+            {
+                try
+                {
+                    Console.Write("Infome um númer: ");
+                    var numero = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i <=1000; i++)
+                    {
+                        Console.WriteLine(numero + " x "+ i + " = " + (numero * i));
+                    }
+                    validar = true;
+                }
+                catch
+                {
+                    Console.Write("Número invalido");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
