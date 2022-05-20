18d0ee4ba1741b0d22531355e38c98be0ac33375
Leonardo Pokrewecky Salvador <leonardo.Pokrewecky@gmail.com>
2022-05-20 19:01:48 -0300

    projeto Exercicio vetor program criado Exercicio 01 concluido

---
+++ Entra21.ExerciciosVetor/Exercicio01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            int[] numeros = new int[3];
+
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                var validar = false;
+                while (validar == false)
+                {
+                    try
+                    {
+                        Console.Write($"Informe o {i + 1}° número de {numeros.Length}: ");
+                        numeros[i] = Convert.ToInt32(Console.ReadLine());
+                        validar = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erro. esse caracter não representa um inteiro");
+                    }
+
+                }
+            }
+            var soma = 0;
+            for (var i = 0; i < numeros.Length; i = i+1)
+            {
+                soma += numeros[i];
+            }
+            Console.Write("A soma de todos os número: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Entra21.ExerciciosVetor/Program.cs
@@ -0,0 +1,78 @@
+﻿using Entra21.ExerciciosVetor;
+
+var loop = true;
+
+while (loop == true)
+{
+    var validar = false;
+    var opcaoDesejada = 0;
+    while (validar == false)
+    {
+
+        try
+        {
+            Console.Clear();
+            Console.WriteLine(@"Opções:
+1 - Exercicio01
+2 - Exercicio02
+3 - Exercicio03
+4 - Exercicio04
+5 - Exercicio05
+6 - Exercicio06
+7 - Exercicio07
+99 - sair");
+
+            Console.Write("Opção desejada: ");
+            opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+            if (opcaoDesejada > 0 && opcaoDesejada < 100)
+            {
+                validar = true;
+            }
+            else
+            {
+                Console.Write("Escolha um número da lista");
+                Console.ReadKey();
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Caracter Invalido");
+            Console.ReadKey();
+        }
+
+        if (opcaoDesejada == 1)
+        {
+            var exercicio01 = new Exercicio01();
+            exercicio01.Executar();
+        }
+        else if (opcaoDesejada == 2)
+        {
+
+        }
+        else if (opcaoDesejada == 3)
+        {
+
+        }
+        else if (opcaoDesejada == 4)
+        {
+
+        }
+        else if (opcaoDesejada == 5)
+        {
+
+        }
+        else if (opcaoDesejada == 6)
+        {
+
+        }
+        else if (opcaoDesejada == 7)
+        {
+
+        }
+        else if (opcaoDesejada == 99)
+        {
+            loop = false;
+        }
+
+    }
+}
