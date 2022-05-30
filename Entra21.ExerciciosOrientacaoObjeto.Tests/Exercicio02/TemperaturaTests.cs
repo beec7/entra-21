a34bd0c776ac25ad65e4fa898a789d7fcba6d2d3
LeonardoPokreweckySalvador <leonardo.pokrewecky@gmail.com>
2022-05-30 20:20:01 -0300

    Intervalo

--- Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio02/TemperaturaTests.cs
+++ Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio02/TemperaturaTests.cs
@@ -17,12 +17,46 @@ namespace Entra21.ExerciciosOrientacaoObjeto.Tests.Exercicio02
             // Arrange
             var temperatura = new Temperatura();
             temperatura.TemperaturaValor = 10;
-            
+
             // Act
             var temperaturaFinal = temperatura.CalcularCelsiusKelvin();
 
             //Assert
             temperaturaFinal.Should().Be(283.15);
+        }
+
+        [Fact]
+        public void Validar_CalcularCelsiusFahrenheit()
+        {
+            // Arrange
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaValor = 10;
+
+            // Act
+            var temperaturaFinal = temperatura.CalcularCelsiusFahrenheit();
+            var valorTemperaturaVerificar = (10 * 1.8) + 32;
+
+            // Assert
+            temperaturaFinal.Should().Be(valorTemperaturaVerificar);
+        }
+
+        [Theory]
+        [InlineData(10, -263.15)]
+        [InlineData(0, -273.15)]
+
+
+        public void Validar_CalcularKelvinCelsius(double temperaturaEntrada, double temperaturaConvertida)
+        {
+            // Arrange
+            var temperatura = new Temperatura();
+            temperatura.TemperaturaValor = temperaturaEntrada;
+
+            // Act
+            var temperaturaFinal = temperatura.CalcularKelvinCelsius();
+
+            // Assert
+            temperaturaFinal.Should().Be(temperaturaConvertida);
+
         }
     }
 }
