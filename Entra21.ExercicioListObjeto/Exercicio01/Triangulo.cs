cb4963010d748e2377ba4ab23c291a69c1506e5c
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-06-09 22:13:31 -0300

    final Aula

---
+++ Entra21.ExercicioListObjeto/Exercicio01/Triangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioListObjeto.Exercicio01
+{
+    internal class Triangulo
+    {
+        public int Lado1;
+        public int Lado2;
+        public int Lado3;
+        public int Codigo;
+
+        public bool ValidorTriangulo()
+        {
+            if (Lado1 != 0)
+            {
+                if (Lado2 != 0)
+                {
+                    if (Lado3 != 0)
+                    {
+                        return true;
+                    }
+                }
+            }  
+            return false;
+        }
+    }
+}
---
+++ Entra21.ExercicioListObjeto/Exercicio01/TrianguloServico.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioListObjeto.Exercicio01
+{
+    internal class TrianguloServico
+    {
+        private List<Triangulo> triangulos = new List<Triangulo>();
+        private int CodigoAtual = 1;
+
+        public bool Adicionar(int lado1, int lado2, int lado3)
+        {
+            Triangulo triangulo = new Triangulo();
+            triangulo.Lado1 = lado1;
+            triangulo.Lado2 = lado2;
+            triangulo.Lado3 = lado3;
+            triangulo.Codigo = CodigoAtual;
+            CodigoAtual += 1;
+            triangulos.Add(triangulo);
+
+            //rever
+            return true;
+
+        }
+
+        public bool Apagar(int codigoApagar)
+        {
+            Triangulo trianguloParaApagar = ObterPorCodigo(codigoApagar);
+
+            return trianguloParaApagar == null
+            ? false
+            : true;
+            //(triangulos.Remove(trianguloParaApagar));
+        }
+        public bool Editar(int codigoAlterar, int lado1, int lado2, int lado3)
+        {
+            Triangulo trianguloParaAlterar = ObterPorCodigo(codigoAlterar);
+
+            if (trianguloParaAlterar == null)
+            {
+                return false;
+            }
+
+            trianguloParaAlterar.Lado1 = lado1;
+            trianguloParaAlterar.Lado2 = lado2;
+            trianguloParaAlterar.Lado3 = lado3;
+
+            return true;
+        }
+        public List<Triangulo> ObterTodos()
+        {
+            return triangulos;
+        }
+
+        public Triangulo ObterPorCodigo(int codigo)
+        {
+            for (int i = 0; i < triangulos.Count; i++)
+            {
+                var trianguloAtual = triangulos[i];
+
+                if (trianguloAtual.Codigo == codigo)
+                {
+                    return trianguloAtual;
+                }
+            }
+            return null;
+        }
+    }
+}
