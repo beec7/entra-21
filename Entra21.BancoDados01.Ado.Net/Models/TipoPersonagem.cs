461b7aa4d489b1a4ddec92972cf22d47ed7b8fd5
leonardo.salvador <leonardo.pokrewecky@gmail.com>
2022-07-14 19:18:09 -0300

    Exemplo professor Form e SQL

---
+++ Entra21.BancoDados01.Ado.Net/Models/TipoPersonagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados01.Ado.Net.Models
+{
+    internal class TipoPersonagem
+    {
+        public int Id { get; set; }
+
+        public string tipo { get; set; }
+
+    }
+}
---
+++ Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
@@ -0,0 +1,35 @@
+﻿using Entra21.BancoDados01.Ado.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados01.Ado.Net.Services
+{
+    //Dois pontos seguindo de uma interface, quer dizer que a classe
+    //Será obrigada a implementar os metodos(Contratos) estabelecidos 
+    //na interface
+    internal class TipoPersonagemService : ITipoPersonagemService
+    {
+        public void Cadastrar(TipoPersonagem tipoPersonagem)
+        {
+            // Sera descrito a implamentacao do contrato definido na interface 
+
+            //Instrnciado um objeto da classe SqlConnection, que permitera fazer selects, inserts, updates, deletes, etc.
+
+            SqlConnection conexao = new SqlConnection();
+
+            //String que contem o caminho para o banco de dados, o que permitira conectar ao banco de dados
+            var connectionString = @""; 
+
+            //Definir o caminho da conexao para o SqlConnection
+            conexao.ConnectionString = connectionString;
+
+            // Abrir a conexao com o banco de dados
+            conexao.Open();
+
+        }
+    }
+}
