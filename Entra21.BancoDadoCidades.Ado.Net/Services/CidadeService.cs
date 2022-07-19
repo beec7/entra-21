975c6d9c41c0c45af6e3fde7a18314ac3c700b80
beec7 <leonardo.pokrewecky@gmail.com>
2022-07-19 00:48:51 -0300

    Cidade Service Adicinado os metodos Cadastrar, Editar, Apagar

--- Entra21.BancoDadoCidades.Ado.Net/Services/CidadeService.cs
+++ Entra21.BancoDadoCidades.Ado.Net/Services/CidadeService.cs
@@ -1,9 +1,5 @@
-﻿using Entra21.BancoDadoCidades.Ado.Net.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entra21.BancoDadoCidades.Ado.Net.DataBase;
+using Entra21.BancoDadoCidades.Ado.Net.Models;
 
 namespace Entra21.BancoDadoCidades.Ado.Net.Services
 {
@@ -11,17 +7,52 @@ namespace Entra21.BancoDadoCidades.Ado.Net.Services
     {
         public void Cadastrar(Cidade cidade)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = @"INSERT INTO cidades(nome, quantidade_habitante, pib, data_hora_fundacao, id_unidade_federativa) VALUES (@NOME, @QUANTIDADE_HABITANTE, @PIB, @DATA_HORA_FUNDACAO, @ID_UNIDADE_FERERATIVA)";
+            comando.Parameters.AddWithValue("@nome", cidade.Nome);
+            comando.Parameters.AddWithValue("@QUANTIDADE_HABITANTE", cidade.QuantidadeHabitante);
+            comando.Parameters.AddWithValue("@PIB", cidade.Pib);
+            comando.Parameters.AddWithValue("@DATA_HORA_FUNDACAO", cidade.DataHoraFundacao);
+            comando.Parameters.AddWithValue("@ID_UNIDADE_FERERATIVA", cidade.UnidadeFederativa.Id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Connection.Close();
+        
         }
 
         public void Editar(Cidade cidade)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = @"UPDATE cidades SET nome = @NOME, quantidade_habitante = @QUANTIDADE_HABITANTE, pib = @PIB, data_hora_fundacao = @DATA_HORA_FUNDACAO, id_unidade_federativa = @ID_UNIDADE_FERERATIVA WHERE id = @ID;";
+            comando.Parameters.AddWithValue("@ID", cidade.Id);
+            comando.Parameters.AddWithValue("@nome", cidade.Nome);
+            comando.Parameters.AddWithValue("@QUANTIDADE_HABITANTE", cidade.QuantidadeHabitante);
+            comando.Parameters.AddWithValue("@PIB", cidade.Pib);
+            comando.Parameters.AddWithValue("@DATA_HORA_FUNDACAO", cidade.DataHoraFundacao);
+            comando.Parameters.AddWithValue("@ID_UNIDADE_FERERATIVA", cidade.UnidadeFederativa.Id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Connection.Close();
         }
-       
+
         public void Apagar(int id)
         {
-            throw new NotImplementedException();
+            var conexao = new Conexao().Conectar();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = @"DELETE FROM cidades WHERE id = @ID";
+
+            comando.Parameters.AddWithValue("@ID", id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Connection.Close();
+
         }
 
         public List<Cidade> ObterTodos()
